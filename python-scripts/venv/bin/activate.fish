610746cec6c95f4d94f198dbeaf8d597cadeadbb
Lizbeth <lcardenase1600@alumno.ipn.mx>
2025-06-23 19:35:24 -0600

    Descarga de videos Bench y Deadlift

--- python-scripts/venv/bin/activate.fish
+++ python-scripts/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/lizcardenas/python-scripts/venv
+set -gx VIRTUAL_ENV /home/lizcardenas/PowerLiftingApp/python-scripts/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
